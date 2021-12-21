30f6714d710db965dbdb9bce84d8363bb5c7e591
Tiago Xavier <tiago.xavier-silva@ecl20.ec-lyon.fr>
2021-12-22 00:03:29 +0100

    Add OCR e dicio

--- pdf_reader V2.py
+++ pdf_reader V2.py
@@ -5,16 +5,28 @@ from pdfminer.pdfpage import PDFPage
 from io import StringIO
 import re
 import os
-import json
-import requests
 import pandas as pd
+import pytesseract 
+import pdf2image
+import pickle
 
+pytesseract.pytesseract.tesseract_cmd = r'C:/Users/Tiago/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
 class PDF():
     def __init__(self,file_path):
-        self.path = file_path      
-        self.text = self.convert_pdf_to_txt(self.path)
-    
+        self.path = file_path
+        self.flag_OCR = False
+        #Tentativa de leitura convencional
+        self.text = self.convert_pdf_to_txt(self.path)   
+        
+        #Caso haja falha na leitura, tentativa por OCR e levanta a flag
+        if len(self.text) < 100:
+            self.flag_OCR = True            
+            self.text =self.convert_pdf_to_txt_OCR(self.path)
+            self.text = '\n'.join(self.text)
+            
+            #! Enviar PDF para uma pasta separada para checagem !
+            
     def convert_pdf_to_txt(self,path, pages=None):
         if not pages:
             pagenums = set()
@@ -32,42 +44,42 @@ class PDF():
         converter.close()
         text = output.getvalue()
         output.close()
-        return text
-
-    def get_email(self):                        
-        email_pos = re.search('[a-z|.|-]+@ecl[0-9][0-9].ec-(\n)*lyon.fr', pdf.text)
-        end = email_pos.span()[-1]
-        start = email_pos.span()[0]
-
-        email = pdf.text[start:end]
-
-        return email
         
-    def get_mises(self):
-        mises_label = ['Mise 1','Mise 2','Mise 3','Mise 4','Mise 5']
-        mises_usuario = []
+        return text    
+            
+    def convert_pdf_to_txt_OCR(self,path): 
+        pages = pdf2image.convert_from_path(pdf_path=path, poppler_path='poppler-21.11.0/Library/bin',dpi=200)
+        content = []     
+            
+        for page in pages:
+            content.append(pytesseract.image_to_string(page, lang='fra')) 
+            
+        return content
+
+    def get_email(self): 
+        email = re.search('[a-z|.|-]+@ecl[0-9][0-9].ec-(\n)*lyon.fr', self.text)    
+        return email.group(0)            
+                           
+    def get_mises(self):          
+        def convert_mises_in_lt_values(lt_mises):
+            mises_values = []
+            for mise in lt_mises:
+                aux_mises = mise[mise.index(':')+1:]
+                
+                if "," in aux_mises:
+                    aux_mises = aux_mises.replace(',','.') 
+                
+                mises_values.append(float(aux_mises))
+            
+            return mises_values            
         
-        for mise_text in mises_label:
-            mise_value = ''
-
-            index =  self.text.find(mise_text) + 9   
-            char =  self.text[index]
-
-            while (char != ' ' and char != '\n'):       
-                mise_value += char
-                index += 1
-                char =  self.text[index]  
-
-            if ',' in mise_value:                    
-                mise_value = mise_value.replace(',','.')                                              
-            mise_value = float(mise_value)
-
-            mises_usuario.append(mise_value)
+        mises = re.findall('Mise [1-5] : [0-9]+[,|.]*[0-9]*', self.text)    
         
-        return mises_usuario
+        return convert_mises_in_lt_values(mises)
+                        
 
-    def get_subjects(self):
-        pos1 = re.search('aléatoirement.', pdf.text)
+    def get_subjects(self):            
+        pos1 = re.search('aléatoirement.', self.text)
         text_choices = pdf.text[pos1.span()[-1]:]
 
         separator = ' '
@@ -102,71 +114,38 @@ class PDF():
         subject = text_choices[end:].split('https', 1)[0]# .replace('\n', '').split('\n', 1)[0].strip()
         tst_list.append(subject)
         subject_dict[i] = subject.split(f'{i} - ')[1].split('\n', 1)[0].replace('\n', '').strip()
-        return subject_dict
-
-
-def getTextOCR(file_path: str) -> str:
-    """
-        Use OCR API to return text of image in pdf file/format
-
-        ## PARAMS:
-            - file_path: string
-
-        ## Returns:
-            - result of API call in string
-    """
-    UserName =  'PETERBANANINHA005'
-    LicenseCode = 'D360C2A3-0877-42BB-9C97-3B550933A647'
-
-    # Extract text with English language by default
-    RequestUrl = "http://www.ocrwebservice.com/restservices/processDocument?language=french&gettext=true&outputformat=txt"
-
-    with open(file, 'rb') as image_file:
-        image_data = image_file.read()
         
-    r = requests.post(RequestUrl, data=image_data, auth=(UserName, LicenseCode))
+        return self.convert_subjects_in_keys(subject_dict)
 
-    if r.status_code == 401:
-        # Please provide valid username and license code
-        print("Unauthorized request")
-        exit()
-
-    # Decode Output response
-    jobj = json.loads(r.content)
-
-    ocrError = str(jobj["ErrorMessage"])
-
-    if ocrError != '':
-            #Error occurs during recognition
-            print ("Recognition Error: " + ocrError)
-            exit()
-
-    return str(jobj["OCRText"][0])
-
+    def convert_subjects_in_keys(self,d):            
+        def get_key(val,my_dict):
+            for key, value in my_dict.items():
+                 if val == value:
+                     return key
+                 
+        file = open("Dicionario.pkl", "rb")
+        dicionario = pickle.load(file)                  
+      
+        for key,value in d.items():
+            pos = re.search(' - [A-Z]-', value)
+            disciplina = value[:pos.span()[-1]-5]
+            
+            codigo = get_key(disciplina,dicionario)
+            
+            d[key] = codigo
+                
+        return d
 
 if __name__ == '__main__':
-    os.chdir(os.getcwd() + '/pdfs')  # folder with all pdfs
 
-    columns = ['Email'] +  [f'Mise {i}' for i in range(1,6)] + [i for i in range(1,58)]
-    df = pd.DataFrame(columns = columns)
-
-    for file in os.listdir(os.getcwd()):
-        path = os.getcwd() + '/' + file
-        pdf = PDF(path)
-
-        if len(pdf.text) < 100:
-            print('Using OCR API to read PDF')
-            continue
-            pdf.text = getTextOCR(path)
-
-        print(file)
-        email = pdf.get_email()
-        mises = pdf.get_mises()
-        subjects = [value for value in pdf.get_subjects().values()]
-
-        if len(subjects) < 57:
-            missing = 57 - len(subjects)
-            subjects += ['-' for i in range(missing)]
-
-        row = pd.Series([email] + mises +  subjects, index=df.columns)
-        df = df.append(row, ignore_index=True)
+    pdf = PDF('pdfs\ellectifs S8.pdf')
+    #text = pdf.convert_pdf_to_txt()
+    #print(pdf.get_mises())
+    #print(pdf.get_email())
+    d = pdf.get_subjects()
+    
+    print(d)
+    
+    
+        
+  
