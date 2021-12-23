be42a3ad2c1e656d2e9abb159bb631434e919528
matheus-machado <mmachadoesilva@gmail.com>
2021-12-23 19:05:33 -0300

    n
    il est a

--- plot_test.py
+++ plot_test.py
@@ -36,6 +36,19 @@ keys = []
 for key, value in dict_AF.items():
     keys.append(key)
     
+#%%
+dict_Mises = {}
+for user in df['ID']: #Mudar o 9 aqui para a quantidade de dados
+    aux = ast.literal_eval(df['Mises'][user-1]) #
+    for position in range(len(aux)):
+        if position in dict_Mises:
+            dict_Mises[position].append(aux[position])
+        else: 
+            dict_Mises[position]=[aux[position]]
+        #dict_AF[df.iloc[student,position]] =  [position]
+keys_mises = []
+for key, value in dict_Mises.items():
+    keys_mises.append(key)
 #%% PLot e save dos histogramas das materias
 
 with PdfPages('histogramas.pdf') as pdf:
@@ -46,12 +59,32 @@ with PdfPages('histogramas.pdf') as pdf:
         locx, labelsx = plt.xticks()
         plt.yticks(np.arange(0, max(loc), step=1))
         #commutes.plot.hist(grid=True, bins=57, rwidth=0.9,color='#607c8e', align = 'mid')
-        plt.xticks(np.arange(1, 57, step=1))
         plt.hist(dict_AF[materia], bins=57, align = 'left')
+        plt.xticks(np.arange(1, 57, step=1))
         #ya = axes.get_yaxis()
         #ya.set_major_locator(MaxNLocator(integer=True))
         plt.title(dicionario[materia], fontsize= 20)
         plt.xlabel('Position', fontsize= 20)
+        plt.ylabel('Quantité de personnes', fontsize= 20)
+        plt.grid(axis='y', alpha=0.75)
+        pdf.savefig()
+
+#%% PLot e save dos histogramas das MIses
+
+with PdfPages('Distribuiçao de mises.pdf') as pdf:
+    for mises in keys_mises:
+        #commutes = pd.Series(dict_AF[materia])
+        plt.figure(figsize=(17,10))
+        loc, labels = plt.yticks()
+        locx, labelsx = plt.xticks()
+        plt.yticks(np.arange(0, max(loc), step=1))
+        #commutes.plot.hist(grid=True, bins=57, rwidth=0.9,color='#607c8e', align = 'mid')
+        plt.hist(dict_Mises[mises], bins=35, align = 'mid')
+        #plt.xticks(np.arange(1, 57, step=1))
+        #ya = axes.get_yaxis()
+        #ya.set_major_locator(MaxNLocator(integer=True))
+        plt.title( 'Mise' + str(mises+1), fontsize= 20)
+        plt.xlabel('Valor atribuido à mise', fontsize= 20)
         plt.ylabel('Quantité de personnes', fontsize= 20)
         plt.grid(axis='y', alpha=0.75)
         pdf.savefig()
